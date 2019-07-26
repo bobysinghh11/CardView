5f5279de021f45327ce638c79072cbce1f590fb1
bobbysingh <Meghsingh@12345>
2019-07-26 08:28:50 +0530

    Initial commit

---
+++ app/src/main/java/com/stuffshuf/cardview/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.stuffshuf.cardview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val courses = genNRandomCourses(10)
+
+        rvCourses.layoutManager = GridLayoutManager(
+            this,
+            2,
+            GridLayoutManager.HORIZONTAL,false
+
+        )
+        rvCourses.adapter = CourseAdapter(courses)
+
+    }
+}
